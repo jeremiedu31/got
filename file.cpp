80bd0dae6daad70556b48b3b6611f0fcf2cb7b07
PrincessDu31 <elise.ritoux@orange.fr>
2017-07-01 21:15:27 +0200

    read file

---
+++ file.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <stdio.h>
+#include <cstdlib>
+#include <fstream>
+#include <vector>
+
+#include "file.h"
+
+#define BUFFER_SIZE 1024
+
+
+vector<string> read (const char * filename) {
+	vector<string> v;
+	if (FILE *fp = fopen(filename, "r"))
+	{
+		char buf[BUFFER_SIZE];
+		int i = 0, j = 0;
+		char str[20] = {""};
+		while (size_t len = fread(buf, 1, sizeof(buf), fp)) {
+			// v.insert(v.end(), buf, buf + len);
+			j = 0;
+
+			for (i = 0; i < len; i++) {
+				// cout << buf[i];
+				// cout << (int)buf[i] << " ";
+
+				if (buf[i] == 13 || buf[i] == 11 || buf[i] == 10) {
+					v.push_back((string)str);
+					for (; j > 0; j--)
+						str[j] = 0;
+				} else {
+					str[j] = buf[i];
+					j++;
+				}
+
+			}
+
+		}
+		
+		fclose(fp);
+	}
+	return v;
+}
+
+
+int main () {
+	vector<string> v = read("fichier1.txt");
+	for (int i = 0; i < v.size(); ++i)
+	{
+		cout << v[i] << endl;
+	}
+
+    return 0;
+}
---
+++ file.h
@@ -0,0 +1,14 @@
+#ifndef FILE_H
+#define FILE_H
+
+#include <fstream>
+#include <cstdlib>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<string> read (string filename);
+
+
+#endif
